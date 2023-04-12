cd4cca6a1b65f93e3e8e8405efd28ca1d9afe12d
ljjs0121 <ljjs1215@gmail.com>
2023-04-12 22:07:11 +0900

    출력해보기

---
+++ src/intro/Hello.java
@@ -0,0 +1,10 @@
+package intro;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, java");
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다");
+	}
+}
---
+++ src/intro/Hello3.java
@@ -0,0 +1,14 @@
+package intro;
+
+public class Hello3 {
+
+	public static void main(String[] args) {
+		// 이름, 전화번호, 이메일, 나이 출력
+		System.out.println("이진서");
+		System.out.println("010-7415-0706");
+		System.out.println("ljjs1215@gmail.com");
+		System.out.println(30);
+		
+	}
+
+}
